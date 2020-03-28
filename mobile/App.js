2516f8cbd20c3cbe8248aec7430419abb5c046e2
linchiyu <32578426+linchiyu@users.noreply.github.com>
2020-03-28 00:49:05 -0300

    dia 4

---
+++ mobile/App.js
@@ -0,0 +1,32 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+//import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
+/*
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start woraking on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
